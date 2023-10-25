0213b4808fedfaab77efa265e4a81b50a9cffb66
Jmalak03 <jossidey@hotmail.es>
2023-10-25 02:10:40 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Beauty_Contest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
