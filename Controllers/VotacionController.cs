96bff96d790336fea50d6f1035d7d0a9ac47df73
Gabriel027-ux <gabrielcalde16@gmail.com>
2023-10-28 22:50:28 -0600

    VotacionController, tiene un error no se como arreglarlo

--- Controllers/VotacionController.cs
+++ Controllers/VotacionController.cs
@@ -1,6 +1,29 @@
-﻿namespace Beauty_Contest.Controllers
+﻿using System.Data.SqlClient;
+using Beauty_Contest.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Beauty_Contest.Controllers
 {
-    public class VotacionController
+    public class VotacionController : Controller
     {
+        private DbContext _context;
+        
+        public VotacionController(DbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Votar(int Id)
+        {
+            var concursante = _context.Concursante.FirstOrDefault(c => c.Id == Id);
+            if (concursante != null)
+            {
+                concursante.Votos++;
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
--- Models/VotacionModel.cs
+++ Models/VotacionModel.cs
@@ -1,6 +1,22 @@
-﻿namespace Beauty_Contest.Models
+﻿using FreeSql.Extensions;
+using Beauty_Contest.Controllers;
+
+namespace Beauty_Contest.Models
 {
     public class VotacionModel
     {
+        public class Concursante
+        {
+            public int ConcursanteId { get; set; }
+            public string Concursantes { get; set; }
+            public int Votos { get; set; }
+        }
+
+        public class Voto
+        {
+            public int VotoId { get; set; }
+            public string ConcursanteId { get; set; }
+            public int UserId { get; set; }
+        }
     }
 }
