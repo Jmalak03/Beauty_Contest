0eec65d5030ef16cfa267f6088fbd6bf3e8ab3bb
Ian08E <146143120+Ian08E@users.noreply.github.com>
2023-10-31 19:24:36 -0600

    Creacion del agregar

---
+++ Controllers/AgregarController.cs
@@ -0,0 +1,26 @@
+﻿using Beauty_Contest.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Beauty_Contest.Controllers
+{
+    public class AgregarController : Controller
+    {
+        private List<AgregarModel> personasList = new List<AgregarModel>() { };
+        public IActionResult Index(AgregarModel persona)
+        {
+            return View("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Index(AgregarModel persona)
+        {
+
+
+            personasList.Add(persona);
+
+            return View("Index");
+
+        }
+    }
+}
+    
---
+++ Models/AgregarModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Beauty_Contest.Models
+{
+    public class AgregarModel
+    {
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime FechaNam { get; set; }
+        public int Edad { get; set; }
+        public string Peso { get; set; }
+        public string Altura { get; set; }
+        public string Nacionalidad { get; set; }
+        public int ModeloId { get; set; }
+    
+    }
+    
+}
+   
